97462654e78879f8eb7fcf92534f9df30ed903d1
Arjun Subedi <arjun@chainintel.ai>
2021-08-01 21:00:27 -0500

    accessibility using react-aria

---
+++ src/accesibility/AccessWrapper.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { FocusScope, useFocusManager } from "react-aria";
+
+export const MenuAccess = (props) => {
+  return (
+    <div role="toolbar">
+      <FocusScope contain autoFocus restoreFocus>
+        {props.children}
+      </FocusScope>
+    </div>
+  );
+};
+
+export const MenuItemAccess = (props) => {
+  let focusManager = useFocusManager();
+  let onKeyDown = (e) => {
+    switch (e.key) {
+      case "ArrowDown":
+        focusManager.focusNext({ wrap: true });
+        break;
+      case "ArrowUp":
+        focusManager.focusPrevious({ wrap: true });
+    }
+  };
+
+  return <button onKeyDown={onKeyDown}>{props.children}</button>;
+};
